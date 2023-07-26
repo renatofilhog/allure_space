57183e96d10c319f9a1fdc577cdbcef5c4cdc1ef
Renato Filho <frenato258@gmail.com>
2023-07-26 14:23:32 -0300

    Iniciado app Usuarios

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
