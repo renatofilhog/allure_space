422e8b3493ad770dc61f2a7fa2b0c4979b1d0f16
Renato Filho <frenato258@gmail.com>
2023-07-30 15:42:05 -0300

    Registro e login templates

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from usuarios.views import login, registrar
 
 urlpatterns = [
-
+      path('login',login, name='login'),
+      path('registrar', registrar, name='registrar'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+def registrar(request):
+    return render(request, 'usuarios/registrar.html')
